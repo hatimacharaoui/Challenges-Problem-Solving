c90f22c9297347ea5d897b1387fa756cbb79a7c9
hatimacharaoui <152444218+hatimacharaoui@users.noreply.github.com>
2025-10-30 17:37:56 +0100

    Défi4.js

--- Défi4.js
+++ Défi4.js
@@ -1,9 +1,13 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-let tab = [1, 2, 2, 3, 3, -4, 5, 5];
-Somme = 0;
+let tab = [1, 2, 2, 3, -4, 4, 5];
+let somme = 0;
 for (let i = 0; i < tab.length; i++) {
-    if (tab[i] > 0) {
-        
-    }
+  if (tab[i] < 0) {
+    break;
+  } else {
+    somme = somme + tab[i];
+  }
 }
+
+console.log(somme);
