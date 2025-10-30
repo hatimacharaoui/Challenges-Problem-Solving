006b1c5a18b900b888de6521e4c211a300aca5a1
hatimacharaoui <152444218+hatimacharaoui@users.noreply.github.com>
2025-10-30 17:32:43 +0100

    Update Défi3.js

--- Défi3.js
+++ Défi3.js
@@ -1,11 +1,18 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
-let tab = [1, 2, 2, 3, 3, 4, 5, 5];
+let tab = [1, 2, 2, 3, 4, 4, 5]
+let tab1 = [];
 for (let i = 0; i < tab.length; i++) {
-    for (let j = 0; j < tab.length; j++) {
-        if (tab[i] == tab[j+1]) {
-         tab.splice(j+1, 1);
+    let exist = false;
+    for (let j = 0; j < tab1.length; j++) {
+        if (tab[i] === tab1[j]) {
+            exist = true;
+            break;
+        }
     }
-    }
+    if (exist == false) {
+            
+            tab1.push(tab[i]);
+        }
 }
-console.log(tab);
+console.log(tab1);
